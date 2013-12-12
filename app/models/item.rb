77b9128639adc62b19574770f557d72e62931a25
Matt Lucas <mattlucas@gmail.com>
2013-12-12 16:01:57 -0500

    testing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
-class Items < ActiveRecord::Base
+class Item < ActiveRecord::Base
   attr_accessible :item_name, :description, :item_parent_id, :created_at, :updated_at
   has_many :items_itineraries
   has_many :itineraries, through: :items_itineraries
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  # describe "GET 'show'" do
+
+  #   it "should be successful" do
+  #     get :show, :id => @user.id
+  #     response.should be_success
+  #   end
+
+  #   it "should find the right user" do
+  #     get :show, :id => @user.id
+  #     assigns(:user).should == @user
+  #   end
+
+  # end
+
+end
