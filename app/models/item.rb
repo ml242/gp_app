29680a84fc9c6adb2436b87063a378c6244427ea
Matt Lucas <mattlucas@gmail.com>
2013-12-13 18:09:55 -0500

    login and tables

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   attr_accessible :name, :description, :img_url, :parent_id, :created_at, :updated_at
-  # has_many :items_itineraries #pretty sure this is not right
+  has_many :items_itineraries #pretty sure this is not right
   has_many :itineraries, through: :items_itineraries
 end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,7 +1,7 @@
 class Itinerary < ActiveRecord::Base
   attr_accessible :user_id, :title, :img_url, :description, :itinerary_parent_id, :created_at, :updated_at
   belongs_to :user
-  # has_many :items_itineraries #same here
+  has_many :items_itineraries #same here
   has_many :items, through: :items_itineraries
 
   def copy
