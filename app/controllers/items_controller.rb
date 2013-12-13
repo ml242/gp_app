e9d4adb4c20b8000b4510d915a113982e2c77352
Matt Lucas <mattlucas@gmail.com>
2013-12-12 20:14:58 -0500

    adding items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,22 +5,29 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item.create[params]
+    @item = Item.new
   end
 
   def create
+    @item = params[:id]
+    @item.save
+    redirect_to itinerary_show_path
   end
 
   def edit
+    @item = params[:id]
   end
 
   def show
+    @item = params[:id]
   end
 
   def update
+    @item = params[:id]
   end
 
   def destroy
+    @item = nil
   end
 
 end
