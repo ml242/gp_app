88467a1b787f907e06e1a89042b7e9d316f68000
Matt Lucas <mattlucas@gmail.com>
2013-12-13 15:40:21 -0500

    TRAVIS VANDERHOOP SAVED ME

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
 
   def after_sign_in_path_for(resource)
     itineraries_path
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 
 end
