9d81d307fa2565e56c6ffea9b48b26df5c9b8a87
Matt Lucas <mattlucas@gmail.com>
2013-12-14 13:33:34 -0500

    omniauth

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '3.2.14'
 gem 'pg'
 gem 'backstretch-rails'
 gem 'omniauth'
+gem 'omniauth-facebook'
+# gem 'omniauth-twitter'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131212203644) do
+ActiveRecord::Schema.define(:version => 20131214181926) do
 
   create_table "items", :force => true do |t|
     t.string   "name"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(:version => 20131212203644) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
