9c9c802a495edd5da6797b47089fe03f7e71598a
Matt Lucas <mattlucas@gmail.com>
2013-12-17 09:23:16 -0500

    making some test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,10 +3,25 @@ require 'spec_helper'
 describe WelcomeController do
 
   describe "GET 'index'" do
-    it "should be successful" do
+    it "can successfully pulls the page" do
       get 'index'
       expect(response).to be_success
     end
   end
 
 end
+
+# describe ItinerariesController do
+
+#   before(:each)
+#     u = User.new(name: matt, password: password, password_confirmation: password, email: matt@matt.com)
+#   end
+
+#   describe "GET 'index'" do
+#     it "successfully pulls the page" do
+#       get 'index'
+#       expect(response).to be_success
+#     end
+#   end
+
+# end
