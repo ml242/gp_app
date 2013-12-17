c98e1055cf89486b45e0d781a9f9f35ca3071f76
Katherine Santiago <santiago.kat@gmail.com>
2013-12-16 21:32:16 -0500

    working on collection select

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,16 +11,15 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(params[:item])
-    if params["itinerary_id"]
+    if params["itinerary_id"] != ""
       @item.itineraries << Itinerary.find(params["itinerary_id"])
       @item.save
       redirect_to "/itineraries/#{params[:itinerary_id]}"
-    elsif
+    else
       @item.save
       redirect_to items_path
-    else
-      render :new
     end
+
   end
 
 
@@ -30,18 +29,21 @@ class ItemsController < ApplicationController
   end
 
   def show
-
     @item = Item.find(params[:id])
-    @itineraries = Itinerary.all
-    @link = Link.create
-    @user = current_user
+    @itineraries = Itinerary.where(user_id: current_user.id)
   end
 
   def update
     @item = Item.find(params[:id])
-    if @item.update_attributes(params[:item])
+    if params["itinerary_id"] != ""
+      @item.itineraries << Itinerary.find(params["itinerary_id"])
+      @item.update_attributes(params[:item])
+      binding.pry
+      @item.save
       redirect_to @item
+      #flash notice
     else
+      @item.update_attributes(params[:item])
       render :edit
     end
   end
